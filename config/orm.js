42d149f71717926e69da7d0538084a9cc57de02f
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-10-02 18:18:06 -0500

    initial commit

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Export the orm object
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,17 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = burgers;
