2a329ed8ecc582cedc5f3c046a5b8e57aa5565ee
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-10-03 13:29:22 -0500

    updated burgers.js

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,7 +1,45 @@
-$('.burger-submit').click(function(e){
-    e.preventDefault();
-    // console.log($('.burger-input').val())
-    $.post('/',{ burger_name: $('.burger-input').val()}, function(err, res){
-
-    })
-})
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-eat").on("click", function(event) {
+      var id = $(this).data("id");
+      var neweat = $(this).data("neweat");
+  
+      var neweatState = {
+        eaten: neweat
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: neweatState
+      }).then(
+        function() {
+          console.log("changed eat to", neweat);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        //eaten: $("[name=eaten]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
